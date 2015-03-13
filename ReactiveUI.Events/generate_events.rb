be417221f8afde1bd1a8d8abcecb4faa14d49424
Haacked <haacked@gmail.com>
2015-03-12 18:47:41 -0700

    Embed the nustache template in the code because something keeps deleting that file

--- ReactiveUI.Events/generate_events.rb
+++ ReactiveUI.Events/generate_events.rb
@@ -6,7 +6,7 @@ puts `xbuild /p:Configuration=Debug "#{proj_dir}/EventBuilder.csproj"`
 
 dlls = ARGV.to_a.map {|x| "\"#{x}\"" }.join ","
 puts dlls
-to_execute = "mono #{proj_dir}/EventBuilder.exe #{dlls} #{proj_dir}/Events.mustache > Events.cs"
+to_execute = "mono #{proj_dir}/EventBuilder.exe #{dlls} > Events.cs"
 #puts to_execute
 `#{to_execute}`
 
