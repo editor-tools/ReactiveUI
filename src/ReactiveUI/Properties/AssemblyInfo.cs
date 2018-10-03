01271e725789a82358d766d5f7589afc5bf3a7b5
Steven Kirk <grokys@users.noreply.github.com>
2018-10-03 16:34:40 +0200

    Update ReactiveUI.Testing for use in GHfVS.

--- src/ReactiveUI/Properties/AssemblyInfo.cs
+++ src/ReactiveUI/Properties/AssemblyInfo.cs
@@ -6,6 +6,11 @@ using System.Runtime.CompilerServices;
 
 //[assembly: InternalsVisibleTo("ReactiveUI.Tests")]
 //[assembly: InternalsVisibleTo("ReactiveUI.Winforms")]
-//[assembly: InternalsVisibleTo("ReactiveUI.Wpf")]
+[assembly: InternalsVisibleTo(@"ReactiveUI.Wpf,PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001007db3b4905d7764" +
+"028219e473e22d01fecdad59831ea65985cf080ffc9b3d444e72bcccb0e0789789df057929675a" +
+"873dcae2f61018f0ded2b390159748aa9615786c7ddc7459f126e622f271e7ca413f886e7e00aa" +
+"e7c65dbcd7b22be90c0c52e191b90279aed4f6468f8fefecd512f1e0a379d87a1d89756f5d330e" +
+"d9de07b7")]
 //[assembly: InternalsVisibleTo("ReactiveUI.XamForms")]
 //[assembly: InternalsVisibleTo("ReactiveUI.AndroidSupport")]
